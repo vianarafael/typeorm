12868ebdad1a089443c8b1cd110a930a7f129714
vianarafael <vianarafaelds@gmail.com>
2020-08-27 14:14:36 +0900

    Finished a basic CRUD API

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,24 @@
 import { Router, Request, Response } from 'express';
 
+import {
+  getTasks,
+  saveTask,
+  getTask,
+  updateTask,
+  finishedTask,
+  removeTask,
+} from './controller/TasksController';
+
 const routes = Router();
 
 routes.get('/', (request: Request, response: Response) => {
-  return response.json({ message: 'Hello World!' });
+  return response.json({ message: 'Home' });
 });
 
+routes.get('/tasks', getTasks);
+routes.get('/tasks/:id', getTask);
+routes.post('/tasks', saveTask);
+routes.put('/tasks/:id', updateTask);
+routes.patch('/tasks/:id', finishedTask); // the patch is preferable when you alter a single register in the DB
+routes.delete('/tasks/:id', removeTask);
 export default routes;
